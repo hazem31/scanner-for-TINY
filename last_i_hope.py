691bad9dd3fb2afe7892425ca9e2bf22be3106f5
hazem <hazemnashar31@gmail.com>
2020-12-15 11:26:54 +0200

    Update last_i_hope.py

--- last_i_hope.py
+++ last_i_hope.py
@@ -84,49 +84,6 @@ class Scanner :
             if flag_of_semicolon == 1:
                 self.tokens.append(token('operator',';'))
 
-        # for toke in self.tokens:
-        #     toke.Print()
-
-        # first = self.lines[3]
-        # first = first.split()
-        # self.remove_all_comments()
-        # i = 0
-        # while i < len(first):
-        #     word = first[i]
-        #     if self.check_comment(word):
-        #         i += 1
-        #         continue
-        #     if '}' in word:
-        #         i += 1
-        #         continue
-        #     if self.check_reserved(word):
-        #         self.tokens.append(token('reserved',word))
-        #         i += 1
-        #         continue
-        #     if self.check_operator(word):
-        #         self.tokens.append(token('operator' , word))
-        #         i += 1
-        #         continue
-        #
-        #     if len(word) == 1:
-        #         if word.isdigit():
-        #             self.tokens.append(token('NUM', word))
-        #         if word.isalpha():
-        #             self.tokens.append(token('ID', word))
-        #         i += 1
-        #         continue
-        #     if len(word) > 1:
-        #         temp_list = self.check_all(word)
-        #         print(temp_list)
-        #         if temp_list is not None:
-        #             for te in temp_list:
-        #                 self.tokens.append(te)
-        #
-        #
-        #     i += 1
-        # for toke in self.tokens:
-        #     toke.Print()
-
 
     #TODO improve this fuction
     def check_comment(self,word):
@@ -186,14 +143,6 @@ class Scanner :
                     return temp_list
                 else:
                     word = new_word
-                    # if len(word) == 1:
-                    #     if self.check_operator(word):
-                    #         temp_list.append(token('operator', word))
-                    #         return temp_list
-                    # else:
-                    #     if word[0] in operators or word[0:2] == ':=':
-                    #         if word[0:2] == ":=":
-                    #             temp_list.
             if word[0:2] == ':=':
                 temp_list.append(token('operator', word[0:2]))
                 if len(word) > 2:
@@ -219,14 +168,6 @@ class Scanner :
                     return temp_list
                 else:
                     word = new_word
-                    # if len(word) == 1:
-                    #     if self.check_operator(word):
-                    #         temp_list.append(token('operator', word))
-                    #         return temp_list
-                    # else:
-                    #     if word[0] in operators or word[0:2] == ':=':
-                    #         if word[0:2] == ":=":
-                    #             temp_list.
             if word[0] in operators:
                 temp_list.append(token('operator', word[0]))
                 if len(word) == 1:
@@ -290,12 +231,6 @@ class Parser:
         self.node_counter += 1
         t = Node('read' + '$' + str(self.node_counter))
         t.shape = 'b'
-        # self.prev1 = self.current_Node
-        # if self.child_or_next == 1:
-        #     self.current_Node.next = t
-        # else:
-        #     self.current_Node.childs.append(t)
-        # self.current_Node = t
         self.next()
         if self.token.type == "ID":
             self.node_counter += 1
@@ -329,12 +264,6 @@ class Parser:
         self.node_counter += 1
         t = Node('write' + '$' + str(self.node_counter))
         t.shape = 'b'
-        # self.prev1 = self.current_Node
-        # if self.child_or_next == 1:
-        #     self.current_Node.next = t
-        # else:
-        #     self.current_Node.childs.append(t)
-        #self.current_Node = t
         self.child_or_next = 0
         self.next()
         e = self.exp()
@@ -379,12 +308,6 @@ class Parser:
             t1 = p1
             self.next()
             t1.set_childs(self.factor())
-            # self.prev1 = self.current_Node
-            # if self.child_or_next == 1:
-            #     self.current_Node.next = t
-            # else:
-            #     self.current_Node.childs.append(t)
-            # self.current_Node = t
             self.child_or_next = 0
         return t1
 
@@ -401,10 +324,6 @@ class Parser:
             t = Node('NUM ' + self.token.value + '$' + str(self.node_counter))
             t.shape = 'c'
             self.prev1 = self.current_Node
-            # if self.child_or_next == 1:
-            #     self.current_Node.next = t
-            # else:
-            #     self.current_Node.childs.append(t)
             self.next()
             return t
 
@@ -413,10 +332,6 @@ class Parser:
             t = Node('ID ' + self.token.value +'$' + str(self.node_counter))
             t.shape = 'c'
             self.prev1 = self.current_Node
-            # if self.child_or_next == 1:
-            #     self.current_Node.next = t
-            # else:
-            #     self.current_Node.childs.append(t)
             self.next()
             return t
 
@@ -603,345 +518,36 @@ class Parser:
             dot.edge(node.text,node.next.text,constraint='false')
 
 
-
-
-
-#
-# class Node:
-#     def __init__(self, t, c, s):
-#         self.token_value = t
-#         self.code_value = c
-#         self.shape = s
-#         self.children = []
-#         self.sibling = None
-#         self.index = None
-#
-#     def set_children(self, y):
-#         try:
-#             assert isinstance(y, list)
-#             for i in y:
-#                 self.children.append(i)
-#         except:
-#             self.children.append(y)
-#
-#     def set_sibling(self, y):
-#         self.sibling = y
-
-#
-# class Parser:
-#     nodes_table = {}
-#     tmp_index = 0
-#     edges_table = []
-#
-#     def __init__(self):
-#         self.token = str
-#         self.tokens_list = ['identifier', ':=',
-#                             'identifier', '+', 'number']
-#         self.code_list = ['x', ':=', 'x', '+', '5']
-#         self.tmp_index = 0
-#         self.token = self.tokens_list[self.tmp_index]
-#         self.parse_tree = None
-#         self.nodes_table = None
-#         self.edges_table = None
-#         self.same_rank_nodes = []
-#
-#     def set_tokens_list_and_code_list(self, x, y):
-#         self.code_list = y
-#         self.tokens_list = x
-#         self.tmp_index = 0
-#         self.token = self.tokens_list[self.tmp_index]
-#
-#     def next_token(self):
-#         if(self.tmp_index == len(self.tokens_list)-1):
-#             return False  # we have reachd the end of the list
-#         self.tmp_index = self.tmp_index + 1
-#         self.token = self.tokens_list[self.tmp_index]
-#         return True
-#
-#     def match(self, x):
-#         if self.token == x:
-#             self.next_token()
-#             return True
-#         else:
-#             raise ValueError('Token Mismatch', self.token)
-#
-#     def statement(self):
-#         if self.token == 'if':
-#             t = self.if_stmt()
-#             return t
-#         elif self.token == 'repeat':
-#             t = self.repeat_stmt()
-#             return t
-#         elif self.token == 'identifier':
-#             t = self.assign_stmt()
-#             return t
-#         elif self.token == 'read':
-#             t = self.read_stmt()
-#             return t
-#         elif self.token == 'write':
-#             t = self.write_stmt()
-#             return t
-#         else:
-#             raise ValueError('SyntaxError', self.token)
-#
-#     def stmt_sequence(self):
-#         t = self.statement()
-#         p = t
-#         while self.token == ';':
-#             q = Node(None, None, None)
-#             self.match(';')
-#             q = self.statement()
-#             if q == None:
-#                 break
-#             else:
-#                 if t == None:
-#                     t = p = q
-#                 else:
-#                     p.set_sibling(q)
-#                     p = q
-#         return t
-#
-#     def factor(self):
-#         if self.token == '(':
-#             self.match('(')
-#             t = self.exp()
-#             self.match(')')
-#         elif self.token == 'number':
-#             t = Node(
-#                 'CONSTANT', '(' + self.code_list[self.tmp_index] + ')', 'o')
-#             self.match('number')
-#         elif self.token == 'identifier':
-#             t = Node('IDENTIFIER',
-#                      '(' + self.code_list[self.tmp_index] + ')', 'o')
-#             self.match('identifier')
-#         else:
-#             raise ValueError('SyntaxError', self.token)
-#             return False
-#         return t
-#
-#     def term(self):
-#         t = self.factor()
-#         while self.token == '*' or self.token == '/':
-#             p = Node(
-#                 'OPERATOR', '(' + self.code_list[self.tmp_index] + ')', 'o')
-#             p.set_children(t)
-#             t = p
-#             self.mulop()
-#             p.set_children(self.factor())
-#         return t
-#
-#     def simple_exp(self):
-#         t = self.term()
-#         while self.token == '+' or self.token == '-':
-#             p = Node(
-#                 'OPERATOR', '(' + self.code_list[self.tmp_index] + ')', 'o')
-#             p.set_children(t)
-#             t = p
-#             self.addop()
-#             t.set_children(self.term())
-#         return t
-#
-#     def exp(self):
-#         t = self.simple_exp()
-#         if self.token == '<' or self.token == '=' or self.token == '>':
-#             p = Node(
-#                 'OPERATOR', '(' + self.code_list[self.tmp_index] + ')', 'o')
-#             p.set_children(t)
-#             t = p
-#             self.comparison_op()
-#             t.set_children(self.simple_exp())
-#         return t
-#
-#     def if_stmt(self):
-#         t = Node('IF', '', 's')
-#         if self.token == 'if':
-#             self.match('if')
-#             t.set_children(self.exp())
-#             self.match('then')
-#             t.set_children(self.stmt_sequence())
-#             if self.token == 'else':
-#                 self.match('else')
-#                 t.set_children(self.stmt_sequence())
-#             self.match('end')
-#         return t
-#
-#     def comparison_op(self):
-#         if self.token == '<':
-#             self.match('<')
-#         elif self.token == '=':
-#             self.match('=')
-#         elif self.token == '>':
-#             self.match('>')
-#
-#     def addop(self):
-#         if self.token == '+':
-#             self.match('+')
-#         elif self.token == '-':
-#             self.match('-')
-#
-#     def mulop(self):
-#         if self.token == '*':
-#             self.match('*')
-#         elif self.token == '/':
-#             self.match('/')
-#
-#     def repeat_stmt(self):
-#         t = Node('REPEAT', '', 's')
-#         if self.token == 'repeat':
-#             self.match('repeat')
-#             t.set_children(self.stmt_sequence())
-#             self.match('until')
-#             t.set_children(self.exp())
-#         return t
-#
-#
-#     def assign_stmt(self):
-#         t = Node('ASSIGN', '(' + self.code_list[self.tmp_index] + ')', 's')
-#         self.match('identifier')
-#         self.match(':=')
-#         t.set_children(self.exp())
-#         return t
-#
-#     def read_stmt(self):
-#         t = Node('READ', '(' + self.code_list[self.tmp_index+1] + ')', 's')
-#         self.match('read')
-#         self.match('identifier')
-#         return t
-#
-#     def write_stmt(self):
-#         t = Node('WRITE', '', 's')
-#         self.match('write')
-#         t.set_children(self.exp())
-#         return t
-#
-#
-#     def create_nodes_table(self, args=None):
-#         if args == None:
-#             self.parse_tree.index = Parser.tmp_index
-#             Parser.nodes_table.update(
-#                 {Parser.tmp_index: [self.parse_tree.token_value, self.parse_tree.code_value, self.parse_tree.shape]})
-#             Parser.tmp_index = Parser.tmp_index+1
-#             if len(self.parse_tree.children) != 0:
-#                 for i in self.parse_tree.children:
-#                     self.create_nodes_table(i)
-#             if self.parse_tree.sibling != None:
-#                 self.create_nodes_table(self.parse_tree.sibling)
-#         else:
-#             args.index = Parser.tmp_index
-#             Parser.nodes_table.update(
-#                 {Parser.tmp_index: [args.token_value, args.code_value, args.shape]})
-#             Parser.tmp_index = Parser.tmp_index+1
-#             if len(args.children) != 0:
-#                 for i in args.children:
-#                     self.create_nodes_table(i)
-#             if args.sibling != None:
-#                 self.create_nodes_table(args.sibling)
-#
-#     def create_edges_table(self, args=None):
-#         if args == None:
-#             if len(self.parse_tree.children) != 0:
-#                 for i in self.parse_tree.children:
-#                     Parser.edges_table.append((self.parse_tree.index, i.index))
-#                 for j in self.parse_tree.children:
-#                     self.create_edges_table(j)
-#             if self.parse_tree.sibling != None:
-#                 Parser.edges_table.append(
-#                     (self.parse_tree.index, self.parse_tree.sibling.index))
-#                 self.same_rank_nodes.append(
-#                     [self.parse_tree.index, self.parse_tree.sibling.index])
-#                 self.create_edges_table(self.parse_tree.sibling)
-#         else:
-#             if len(args.children) != 0:
-#                 for i in args.children:
-#                     Parser.edges_table.append((args.index, i.index))
-#                 for j in args.children:
-#                     self.create_edges_table(j)
-#             if args.sibling != None:
-#                 Parser.edges_table.append((args.index, args.sibling.index))
-#                 self.same_rank_nodes.append([args.index, args.sibling.index])
-#                 self.create_edges_table(args.sibling)
-#
-#     def run(self):
-#         self.parse_tree = self.stmt_sequence()  # create parse tree
-#         self.create_nodes_table()  # create nodes_table
-#         self.create_edges_table()  # create edges_table
-#         self.edges_table = Parser.edges_table  # save edges_table
-#         self.nodes_table = Parser.nodes_table  # save nodes_table
-#         if self.tmp_index == len(self.tokens_list)-1:
-#             print('success')
-#         elif self.tmp_index < len(self.tokens_list):
-#             raise ValueError('SyntaxError', self.token)
-#
-#     def clear_tables(self):
-#         self.nodes_table.clear()
-#         self.edges_table.clear()
-
-# p = Parser()
-# p.run()
-
-
 f = open("scan.txt", "r")
 
 sc = Scanner(f)
 sc.get_all_tokens()
 
-imp_list = []
-imp_list1 = []
-for toke in sc.tokens:
-    if toke.type == 'reserved':
-        imp_list.append(toke.value)
-        imp_list1.append(" ")
-    elif toke.type == 'ID':
-        imp_list.append('identifier')
-        imp_list1.append(toke.value)
-    elif toke.type == 'NUM':
-        imp_list.append('number')
-        imp_list1.append(toke.value)
-    else:
-        imp_list.append(toke.value)
-        imp_list1.append(toke.value)
-
-
-for i in range(len(imp_list)):
-    print(imp_list[i])
-    print(imp_list1[i])
-    print("#######################3")
+# imp_list = []
+# imp_list1 = []
+# for toke in sc.tokens:
+#     if toke.type == 'reserved':
+#         imp_list.append(toke.value)
+#         imp_list1.append(" ")
+#     elif toke.type == 'ID':
+#         imp_list.append('identifier')
+#         imp_list1.append(toke.value)
+#     elif toke.type == 'NUM':
+#         imp_list.append('number')
+#         imp_list1.append(toke.value)
+#     else:
+#         imp_list.append(toke.value)
+#         imp_list1.append(toke.value)
 #
-# p1 = Parser()
-# p1.set_tokens_list_and_code_list(imp_list,imp_list1)
-# p1.run()
 #
+# for i in range(len(imp_list)):
+#     print(imp_list[i])
+#     print(imp_list1[i])
+#     print("#######################3")
+
 
 p = Parser(sc.tokens)
 v = p.start_fun()
-#p.consume_token()
 p.draw(v)
-
-
-# for imp in imp_list:
-#     print(imp)
-
-# ps = Parser(sc.tokens)
-# ps.consume_token()
-# ps.draw(ps.Parse_tree)
 dot.format = 'png'
 dot.view()
-# dot = Digraph()
-# # Add nodes 1 and 2
-#
-#
-#
-# dot.node('1','a7a\n hello')
-# dot.node('2')
-#
-#
-# # Add edge between 1 and 2
-# dot.edge('1','2')
-#
-# # Visualize the graph
-#
-# dot.format = 'png'
-#
-# dot.render('test-output/round-table.gv' , view=True)
-#
-# print('working')
